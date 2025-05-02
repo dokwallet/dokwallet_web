d15e40d527f763d5511f930fc434716463477335
Divyang Khatri <59201976+divyangkhatri@users.noreply.github.com>
2025-05-02 20:56:49 +0530

    Divyang/google site verification (#4)
    * - implemented google site verification
    
    * - fixed bugs
    
    * - move wl to server side
    
    * fixed as per code rabbit
    
    * remove set WL
    
    * fixed as per code rabbit
    
    * - fixed host header

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,9 @@ import ThemeProvider from 'theme/ThemeContext';
 import React from 'react';
 import {NextIntlClientProvider} from 'next-intl';
 import {getLocale, getMessages} from 'next-intl/server';
+import {getWhiteLabelInfo} from 'dok-wallet-blockchain-networks/service/dokApi';
+import {headers} from 'next/headers';
+import {isLocaleSet} from 'utils/updateLocale';
 
 const roboto = Roboto({
   weight: ['400', '500', '700'],
@@ -18,27 +21,63 @@ const roboto = Roboto({
 export default async function RootLayout({children}) {
   let locale;
   let messages;
+  let wlData;
 
   try {
-    locale = await getLocale();
-    messages = await getMessages();
+    const headersList = await headers();
+    const host = headersList.get('x-forwarded-host') ?? headersList.get('host');
+    const firstHost = host.split(':')[0];
+    const resp = await getWhiteLabelInfo(firstHost);
+    wlData = resp?.data;
+  } catch (error) {
+    console.error('Error in getting white label:', error);
+  }
+
+  try {
+    const isLocalExist = await isLocaleSet();
+    if (isLocalExist) {
+      locale = await getLocale();
+    } else {
+      locale = wlData?.defaultLocale || 'en';
+    }
+    messages = await getMessages({locale});
   } catch (error) {
     console.error('Error loading locale or messages:', error);
     locale = 'en';
     messages = {};
   }
 
+  const googleSiteVerification = wlData?.metadata?.google_site_verification;
+  const title = wlData?.title || 'Dok Wallet';
+  const appIcon = wlData?.appIcon?.light ?? '/dokwallet.ico';
   return (
     <html lang={locale}>
-      <ThemeProvider>
-        <body className={roboto.variable}>
-          <NextIntlClientProvider locale={locale} messages={messages}>
-            <StateProvider>
-              <AppRouting>{children}</AppRouting>
-            </StateProvider>
-          </NextIntlClientProvider>
+      <head>
+        {typeof googleSiteVerification === 'string' &&
+          !!googleSiteVerification && (
+            <meta
+              name='google-site-verification'
+              content={googleSiteVerification}
+            />
+          )}
+        <link rel='icon' href={appIcon} />
+        <title>{title}</title>
+      </head>
+      {wlData ? (
+        <ThemeProvider>
+          <body className={roboto.variable}>
+            <NextIntlClientProvider locale={locale} messages={messages}>
+              <StateProvider>
+                <AppRouting wlData={wlData}>{children}</AppRouting>
+              </StateProvider>
+            </NextIntlClientProvider>
+          </body>
+        </ThemeProvider>
+      ) : (
+        <body>
+          <h3 style={{textAlign: 'center', flex: 1}}>Something went wrong</h3>
         </body>
-      </ThemeProvider>
+      )}
     </html>
   );
 }
